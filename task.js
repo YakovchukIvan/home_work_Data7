5ba9b8905a5c006121226de717ac34639a67d10a
litvinovjobs@gmail.com <litvinovjobs@gmail.com>
2023-05-04 21:53:20 +0300

    finish task7 date

--- task.js
+++ task.js
@@ -3,16 +3,17 @@
 const outTask1 = document.querySelector(".outTask1")
 const outTask2 = document.querySelector(".outTask2")
 const outTask3 = document.querySelector(".outTask3")
+const outTask4 = document.querySelector(".outTask4")
 
 
-const taskInput3 = document.querySelector(".taskInput3")
+const inputTask3 = document.querySelector(".inputTask3")
 const taskBtn3 = document.querySelector(".taskBtn3")
 
-
+const inputTask4 = document.querySelector(".inputTask4")
+const taskBtn4 = document.querySelector(".taskBtn4")
 
 
 function task1 () {
-
     const today = new Date();
     const day = today.getDate().toString().padStart(2, '0'); // padStart якщо день має менше 2 символів (тобто менше 10), тоді перед днем додається 0
     const monthNames = ['січня', 'лютого', 'березня', 'квітня', 'травня', 'червня', 'липня', 'серпня', 'вересня', 'жовтня', 'листопада', 'грудня'];
@@ -23,13 +24,13 @@ function task1 () {
     
     outTask1.style.border = "1px solid #fff";
     outTask1.textContent = formattedDate;
-
 }
 task1 ()
 
+
+
 function task2() {
     const data = new Date();
-
     const day = data.getDate();
     const month = data.getMonth() +1;
     const year = data.getFullYear();
@@ -43,18 +44,62 @@ function task2() {
     outTask2.style.border = "1px solid #fff";
     outTask2.textContent = hours + " Пройшло годин з 1970-01-01";
 }
-
 task2()
 
 
-function task3() {
 
+function task3() {
     taskBtn3.addEventListener("click", () => {
-    
-        console.log(taskInput3);
-    
+        const birthday = new Date(inputTask3.value);
+        const today = new Date();
+        const diff = birthday.getTime() - today.getTime();
+        const diffDays = Math.ceil(diff / (1000 * 60 * 60 * 24));
+        
+        console.log(birthday.getTime());
+        console.log(today.getTime());
+
+        console.log("birthday", birthday);
+        console.log("today", today);
+        console.log("diff", diff);
+        console.log("diffDays", diffDays);
+        
+        if (diffDays < 0) {
+            outTask3.textContent = "День народження вже пройшов";
+            outTask3.style.border = "1px solid #fff";
+        } else if (diffDays === 0) {
+            outTask3.textContent = "Сьогодні Ваш день народження!";
+            outTask3.style.border = "1px solid #fff";
+        } else {
+            outTask3.textContent = `До дня народження залишилось ${diffDays} днів`;
+            outTask3.style.border = "1px solid #fff";
+        }
+   
+    })
+}
+task3()
+
+
+
+function task4() {
+    taskBtn4.addEventListener("click", () => {
+        const data = new Date(inputTask4.value);
+        console.log(data);
+        let result = data.getDay();
+
+        console.log(inputTask4.value);
+        if(result === 6 || result === 0) {
+            outTask4.textContent = "Вихідний";
+            outTask4.style.border = "1px solid #fff";
+        } else if (result === 1) {
+            outTask4.textContent = "Буде важко ... понеділок ...";
+            outTask4.style.border = "1px solid #fff";
+        } else {
+            outTask4.textContent = "Будьний день";
+            outTask4.style.border = "1px solid #fff";
+        }
+
+        
     })
 
 }
-
-task3()
+task4()
